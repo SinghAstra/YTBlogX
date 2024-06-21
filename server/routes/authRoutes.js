28818acf999ab605a2e79aeab2c50ea5842d998f
Astra <abhaypratapsinghwd@gmail.com>
2024-06-21 12:25:32 +0530

    Register Controller

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
+import upload from "../config/multer.js";
+import { registerController } from "../controllers/authController.js";
 const router = express.Router();
 
-router.get("/register", (req, res) => {
-  res.json({ message: "Register Success" });
-});
+router.get("/register", upload.single("picture"), registerController);
 
 export default router;
