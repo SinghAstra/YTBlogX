89b48531ddad15ea7ecb8478d599316ee4e60c7e
Astra <abhaypratapsinghwd@gmail.com>
2024-06-24 10:23:01 +0530

    Register Form UI

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import user from "../assets/user.png";
 import "./Register.css";
 
 const Register = () => {
@@ -11,6 +12,9 @@ const Register = () => {
     location: "",
     occupation: "",
   });
+
+  const [profilePicture, setProfilePicture] = useState(null);
+  console.log("profilePicture is ", profilePicture);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -25,36 +29,81 @@ const Register = () => {
     console.log(formData);
   };
 
+  const handlePictureChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setProfilePicture(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <div className="register-form-container">
       <form onSubmit={handleSubmit} className="register-form">
         <h1>Register</h1>
-        <div className="input-container">
+        <div className="avatar-container">
           <input
-            type="text"
-            name="firstName"
-            value={formData.firstName}
-            onChange={handleChange}
-            required
+            type="file"
+            accept="image/*"
+            style={{ display: "none" }}
+            id="fileInput"
+            onChange={handlePictureChange}
           />
-          <label htmlFor="firstName">First Name</label>
+          <label htmlFor="fileInput">
+            <div className="profile-picture-container">
+              {profilePicture ? (
+                <img
+                  src={profilePicture}
+                  alt="Profile"
+                  className="profile-picture"
+                />
+              ) : (
+                <img
+                  src={user}
+                  alt="Profile"
+                  className="profile-picture-placeholder"
+                />
+              )}
+            </div>
+          </label>
+        </div>
+        <div className="input-row">
+          <div className="input-container">
+            <input
+              type="text"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleChange}
+              autoComplete="off"
+              id="firstName"
+              required
+            />
+            <label htmlFor="firstName">First Name</label>
+          </div>
+          <div className="input-container">
+            <input
+              type="text"
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleChange}
+              autoComplete="off"
+              id="lastName"
+              required
+            />
+            <label htmlFor="lastName">Last Name</label>
+          </div>
         </div>
         <div className="input-container">
           <input
             type="text"
-            name="lastName"
-            value={formData.lastName}
-            onChange={handleChange}
-            required
-          />
-          <label htmlFor="lastName">Last Name</label>
-        </div>
-        <div className="input-container">
-          <input
-            type="email"
             name="email"
             value={formData.email}
             onChange={handleChange}
+            autoComplete="off"
+            id="email"
             required
           />
           <label htmlFor="email">Email</label>
@@ -65,26 +114,20 @@ const Register = () => {
             name="password"
             value={formData.password}
             onChange={handleChange}
+            autoComplete="off"
+            id="password"
             required
           />
           <label htmlFor="password">Password</label>
         </div>
-        {/* <div className="input-container">
-          <input
-            type="file"
-            id="picture"
-            name="picture"
-            onChange={(e) =>
-              setFormData({ ...formData, picture: e.target.files[0] })
-            }
-          />
-        </div> */}
         <div className="input-container">
           <input
             type="text"
             name="location"
             value={formData.location}
             onChange={handleChange}
+            autoComplete="off"
+            id="location"
             required
           />
           <label htmlFor="location">Location</label>
@@ -95,6 +138,8 @@ const Register = () => {
             name="occupation"
             value={formData.occupation}
             onChange={handleChange}
+            autoComplete="off"
+            id="occupation"
             required
           />
           <label htmlFor="occupation">Occupation</label>
@@ -108,3 +153,14 @@ const Register = () => {
 };
 
 export default Register;
+
+// {/* <div className="input-container">
+//   <input
+//     type="file"
+//     id="picture"
+//     name="picture"
+//     onChange={(e) =>
+//       setFormData({ ...formData, picture: e.target.files[0] })
+//     }
+//   />
+// </div> */}
