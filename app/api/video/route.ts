c2251e433ecf93a85386c67257455ff05f870da6
Astra <abhaypratapsinghwd@gmail.com>
2025-03-14 09:40:18 +0530

    Fetch Videos api route

---
+++ app/api/video/route.ts
@@ -0,0 +1,33 @@
+import { authOptions } from "@/lib/auth-options";
+import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return NextResponse.json(
+        { message: "Sign In to Get Started" },
+        { status: 401 }
+      );
+    }
+
+    const videos = await prisma.video.findMany({
+      where: {
+        userId: session.user.id,
+      },
+    });
+
+    return NextResponse.json({ videos }, { status: 201 });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log("error.stack is ", error.stack);
+      console.log("error.message is ", error.message);
+    }
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
--- components/dashboard/left-sidebar.tsx
+++ components/dashboard/left-sidebar.tsx
@@ -1,9 +1,54 @@
-import React from "react";
+import { Video } from "@prisma/client";
+import React, { useEffect, useState } from "react";
+import { toast } from "sonner";
 
 export function LeftSidebar() {
+  const [videos, setVideos] = useState<Video[]>();
+  const [isFetchingVideos, setIsFetchingVideos] = useState<boolean>(true);
+  const [message, setMessage] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function fetchVideos() {
+      try {
+        const response = await fetch("/api/video");
+        const data = await response.json();
+        if (!response.ok) {
+          throw new Error(data.message);
+        }
+        setVideos(data.videos);
+      } catch (error) {
+        if (error instanceof Error) {
+          console.log("error.stack is ", error.stack);
+          console.log("error.message is ", error.message);
+        }
+        setMessage("Check Your Network Connection");
+      } finally {
+        setIsFetchingVideos(false);
+      }
+    }
+    fetchVideos();
+  }, []);
+
+  useEffect(() => {
+    if (!message) return;
+    toast(message);
+    setMessage(null);
+  }, [message]);
   return (
     <div className="fixed inset-y-0 left-0 w-96 bg-background border-r pt-16 flex flex-col h-screen">
-      This is left Sidebar
+      {isFetchingVideos ? (
+        <p>Fetching Videos</p>
+      ) : videos && videos.length > 0 ? (
+        videos.map((video) => {
+          return (
+            <div key={video.id} className="border-b border-dotted py-4">
+              <h3>{video.title}</h3>
+            </div>
+          );
+        })
+      ) : (
+        <p>You have no videos Get Started.</p>
+      )}
     </div>
   );
 }
