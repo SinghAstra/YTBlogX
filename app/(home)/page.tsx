de393c710fa3b4fcd69cef783bb0f137da7b3462
Astra <abhaypratapsinghwd@gmail.com>
2025-04-02 17:54:52 +0530

    Updated Font Family and Loading Fallback.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -57,10 +57,10 @@ const HomePage = () => {
                 <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
               </span>
             </button>
-            <h1 className="text-foreground text-center py-6 text-3xl height font-medium text-balance sm:text-4xl md:text-5xl lg:text-7xl  w-full">
-              Transforming <br />
+            <h1 className="text-foreground text-center py-6 text-5xl font-medium text-balance sm:text-6xl md:text-7xl lg:text-8xl  w-full">
+            Transforming <br />
               <span className="text-transparent bg-gradient-to-r from-violet-500 to-fuchsia-500 bg-clip-text inline-bloc">
-                Youtube Learning
+               Youtube Learning
               </span>
             </h1>
 
--- components/provider/provider.tsx
+++ components/provider/provider.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { siteConfig } from "@/config/site";
 import { SessionProvider } from "next-auth/react";
+import Image from "next/image";
 import React, { Suspense } from "react";
 import { VideoProvider } from "../context/video";
 
@@ -8,9 +10,26 @@ interface ProvidersProps {
   children: React.ReactNode;
 }
 
+const LoadingFallback = () => {
+  return (
+    <div className="min-h-screen flex flex-col gap-4 items-center justify-center">
+      <div className="flex gap-4">
+        <Image
+          src={"/favicon.ico"}
+          width={48}
+          height={48}
+          alt={siteConfig.name}
+        />
+        <p className="text-5xl tracking-wide">{siteConfig.name}</p>
+      </div>
+      <p className="text-xl tracking-wide">{siteConfig.description}</p>
+    </div>
+  );
+};
+
 const Providers = ({ children }: ProvidersProps) => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<LoadingFallback />}>
       <SessionProvider>
         <VideoProvider>
           <div>{children}</div>
