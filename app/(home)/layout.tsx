9f59d8858ce381ec6cbd7434372dd174aa3af499
Astra <abhaypratapsinghwd@gmail.com>
2025-04-04 18:49:53 +0530

    Fixed UI bug

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -11,12 +11,12 @@ interface HomeLayoutProps {
 const HomeLayout = async ({ children }: HomeLayoutProps) => {
   const session = await getServerSession(authOptions);
   return (
-    <>
-      <div className="absolute inset-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:3rem_3rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] h-full z-[-1]" />
+    <div className="relative z-0">
+      <div className="absolute top-0 inset-x-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:3rem_3rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] h-[100vh] z-[-1]" />
       <Navbar user={session?.user} />
-      <main className="mt-20 mx-auto w-full ">{children}</main>
+      <main className="mt-12 pt-12 mx-auto w-full ">{children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
 
