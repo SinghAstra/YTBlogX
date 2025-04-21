35cf9a411e8e80cb88d29d790bf5937952dce415
Astra <abhaypratapsinghwd@gmail.com>
2025-04-22 05:26:40 +0530

    Loading Fallback Props

--- components/provider/provider.tsx
+++ components/provider/provider.tsx
@@ -3,30 +3,36 @@
 import { siteConfig } from "@/config/site";
 import { SessionProvider } from "next-auth/react";
 import Image from "next/image";
-import React, { Suspense } from "react";
+import React, { ReactNode, Suspense } from "react";
 import { VideoProvider } from "../context/video";
 
 interface ProvidersProps {
-  children: React.ReactNode;
+  children: ReactNode;
 }
 
 const LoadingFallback = () => {
   return (
-    <div className="min-h-screen flex flex-col gap-4 items-center justify-center">
-      <div className="flex gap-4">
-        <Image
-          src={"/favicon.ico"}
-          width={48}
-          height={48}
-          alt={siteConfig.name}
-        />
-        <p className="text-5xl tracking-wide">{siteConfig.name}</p>
+    <div className="min-h-screen flex items-center justify-center animate-fade-in">
+      <div className="flex flex-col items-center gap-4 lg:max-w-3xl">
+        <div className="flex gap-4 items-center">
+          <Image
+            src={"/favicon.ico"}
+            width={48}
+            height={48}
+            alt={siteConfig.name}
+          />
+          <p className="text-5xl tracking-wide relative">
+            {siteConfig.name}
+            <span className="absolute -bottom-2 left-0 w-full h-0.5 bg-primary animate-width-grow"></span>
+          </p>
+        </div>
+        <p className="text-xl tracking-wide flex items-center text-center ">
+          {siteConfig.description}
+        </p>
       </div>
-      <p className="text-xl tracking-wide">{siteConfig.description}</p>
     </div>
   );
 };
-
 const Providers = ({ children }: ProvidersProps) => {
   return (
     <Suspense fallback={<LoadingFallback />}>
--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,7 @@ import { SiteConfig } from "@/interfaces/site";
 export const siteConfig: SiteConfig = {
   name: "YTBlogX",
   description:
-    "Generate professional blog posts from YouTube video content automatically using cutting-edge AI technology.",
+    "Generate professional blog posts from YouTube video content automatically using cutting-edge AI technology",
   url: "https://ytblogx.vercel.app",
   links: {
     twitter: "https://x.com/XSinghAstra",
